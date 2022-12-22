17ade5075ec16417f965c5f4a5c15ac2b7d28e03
1alberto994 <alby94ct@gmail.com>
2022-12-22 16:23:17 +0100

    esercizio mail completato

--- Mail/scripts.js
+++ Mail/scripts.js
@@ -1,22 +1,32 @@
 const lista=["Alberto","Davide","Marco","Matteo"];
 console.log(lista);
-lista.includes(0)
+// lista.includes(0)
 const myChoose=prompt("inserisci email");
-console.log(myChoose)
+console.log(myChoose);
 alert(myChoose);
-
-
-
-if(lista.includes[myChoose]){
-    alert("email non valida")
+const trovato=lista.includes(myChoose);
+if(trovato==false){
+    alert("email non valida");
 }
 else{
-    alert( "email valida")
+    alert( "email valida");
 }
 
 
-
-
+// for(let i=0;i<lista.length;i++){
+//     if(lista[i]==myChoose){
+//         alert("email  valida");
+//     }
+    
+ 
+// }
+// const stopCiclo=lista;
+//     for(let i=0;i>=stopCiclo;i++){
+//         if(stopCiclo[i]==myChoose){
+//             alert("email non valida")
+//         }
+           
+//     }
 // comporre una  lista tramite array
 // creare un prompt con scritto inserisci email
 // se la lista è diverso dalla variabile che ho creato tramite il prompt, l email non è valida
