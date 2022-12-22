bf3e92d91ba7c86d3485c8298db3c639f1154668
1alberto994 <alby94ct@gmail.com>
2022-12-22 15:16:32 +0100

    commenti mail

--- Mail/scripts.js
+++ Mail/scripts.js
@@ -1,13 +1,23 @@
 const lista=["Alberto","Davide","Marco","Matteo"];
 console.log(lista);
+lista.includes(0)
 const myChoose=prompt("inserisci email");
 console.log(myChoose)
-alert([myChoose]);
+alert(myChoose);
 
 
-if(isNaN(myChoose)){
+
+if(lista.includes[myChoose]){
     alert("email non valida")
 }
 else{
-    alert(lista[myChoose], "prosegui con l'iscrizione")
+    alert( "email valida")
 }
+
+
+
+
+// comporre una  lista tramite array
+// creare un prompt con scritto inserisci email
+// se la lista è diverso dalla variabile che ho creato tramite il prompt, l email non è valida
+// altrimenti l email è valida
